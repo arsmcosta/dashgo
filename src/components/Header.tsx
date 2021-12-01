e9d9a0ab2229b9a87e8116a219db2ddab0cdd9b3
Renato Costa <arsmcosta@gmail.com>
2021-11-30 23:37:11 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 export function Header() {
   return (
     <Flex
@@ -41,6 +45,31 @@ export function Header() {
           _placeholder={{ color: "gray.400" }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box mr='4' textAlign='right'>
+            <Text>Renato Costa</Text>
+            <Text color="gray.300" fontSize="small">
+              arsmcosta@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size='md' name='Renato Costa' src='https://github.com/arsmcosta.png'/>
+        </Flex>
       </Flex>
     </Flex>
   );
