68458900b0d068939e6e72dfe00c920cbc139aa2
Renato Costa <arsmcosta@gmail.com>
2022-01-04 17:06:12 -0300

    Componente de paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -116,7 +116,11 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination 
+                totalCountRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
